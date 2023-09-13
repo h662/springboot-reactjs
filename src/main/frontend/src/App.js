4d9d34288c9cfda56bee698d9e89426845aeeb42
h662 <h662hong@gmail.com>
2023-09-13 18:04:29 +0900

    first commit

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [hello, setHello] = useState("");
+
+  const getHello = async () => {
+    try {
+      const response = await axios.get("/api/hello");
+
+      setHello(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getHello();
+  }, []);
+
+  return <div>{hello}</div>;
+}
+
+export default App;
